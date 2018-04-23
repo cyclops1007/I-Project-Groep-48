8f6c53aea807622ccee9ddef287492fc2c878f1d
cyclops1007 <kayverhaegh1007@gmail.com>
2018-04-23 14:47:50 +0200

    Template gemaakt

---
+++ Website/EenmaalAndermaal/Template.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kay
+ * Date: 23-4-2018
+ * Time: 9:58
+ */
+// Eventuele includes/ requires.
+// Eventuele sessions/ cookies.
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Normal CSS -->
+    <link rel="stylesheet" type="text/css" href="../css/Template.css">
+    <!-- Bootstrap CSS -->
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+</head>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+<body class="text-white bg-dark">
+<nav id="navbar" class="navbar navbar-expand-lg navbar-dark">
+    <img src="../images/???.png">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown ">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Disabled</a>
+            </li>
+        </ul>
+        <nav aria-label="breadcrumb">
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder=". . ." aria-label="Search">
+            <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+        </form>
+    </div>
+</nav>
+<nav class="...">
+<ol id="Breadcrumb" class="breadcrumb">
+    <li class="breadcrumb-item active" aria-current="page"><a href="#">Home > </a></li>
+</ol>
+</nav>
+<div id="text-container" class="container w-50 h-50 rounded ">
+    <h1>TEST</h1>
+    <p>TESTTESTTESTTESTTESTTESTTESTTESTTESTTESTT ESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTE STTESTTESTTESTTESTTEST TESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTE STTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTEST</p>
+</div>
+<!--Footer-->
+<footer class="page-footer font-small indigo pt-0">
+    <hr class="rgba-white-light" style="margin: 0 15%;">
+    <!--Grid row-->
+    <div class="row pt-5 mb-3 text-center d-flex justify-content-center">
+        <!--Grid columns-->
+        <div class="col-md-2 mb-3">
+            <h6 class="text-uppercase font-weight-bold">
+                © 2018 Copyright
+            </h6>
+        </div>
+        <div class="col-md-2 mb-3">
+            <h6 class="text-uppercase font-weight-bold">
+                <a href="#!">voorwaarden</a>
+            </h6>
+        </div>
+        <div class="col-md-2 mb-3">
+            <h6 class="text-uppercase font-weight-bold">
+                <a href="#!">Contact</a>
+            </h6>
+        </div>
+        <!--Grid columns-->
+    </div>
+    <!--Grid row-->
+</footer>
+<!--/Footer-->
+</body>
+</html>
