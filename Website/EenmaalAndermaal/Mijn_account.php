6a0e1180bdf45e0a5953908fd8739cbfcafb054b
GodspeedK <emerson_martina@yahoo.com>
2018-04-25 11:16:05 +0200

    Revert "Account page changes"
    This reverts commit 2c78713ee179656dabfba8e0a51f06383eef7cd3.

--- Website/EenmaalAndermaal/Mijn_account.php
+++ Website/EenmaalAndermaal/Mijn_account.php
@@ -5,10 +5,16 @@
  * Date: 24-4-2018
  * Time: 10:37
  */
+<<<<<<< HEAD
 session_start();
 include 'Template.php';
 if(isset($_SESSION["change"])){
     $_SESSION["change"] = $_POST["change"];
+=======
+include 'Template.php';
+if(isset($_SESSION["change"])){
+    $_SESSION["change"] = $_POST["Change"];
+>>>>>>> parent of 2c78713... Account page changes
 }else{
     $_SESSION["change"] = NULL;
 }
@@ -26,7 +32,11 @@ echo "</pre>";
             <h1>Mijn account</h1><br>
             <?php
             if($_SESSION['change'] == 0) {
+<<<<<<< HEAD
                  echo "<h2>Gebruikersnaam:</h2>" . $username;
+=======
+                echo "<h2>Gebruikersnaam:</h2>" . $username;
+>>>>>>> parent of 2c78713... Account page changes
                 echo "<h2>Naam: </h2>" . $firstname . $lastname;
                 echo "<h2>Adres:</h2>" . $address;
                 echo "<h2>Adres-2:</h2>" . $address_2;
@@ -36,6 +46,7 @@ echo "</pre>";
                 echo "<h2>Geboortedatum:</h2>" . $date;
                 echo "<h2>E-mail:</h2>" . $mail;
             }elseif ($_SESSION['change'] == 1){?>
+<<<<<<< HEAD
                 <form action="Mijn_account.php" method="POST">
                     <div class="form-group">
                         <label for="firstname">Gebruikersnaam:</label>
@@ -58,11 +69,39 @@ echo "</pre>";
                         <input class="form-control" type="text" name="city" id="city"><br>
                         <label for="mail">E-mail:</label>
                         <input class="form-control" type="text" name="mail" id="mail"><br><br>
+=======
+                <form action="welcome.php" method="post">
+                    <div class="form-group">
+                        <label>Gebruikersnaam:</label>
+                        <input class="form-control" type="text" name="firstname"><br>
+                        <label>Adres:</label>
+                        <input class="form-control" type="text" name="lastname"><br>
+                        <label>Adres-2:</label>
+                        <input class="form-control" type="text" name="username"><br>
+                        <label>Postcode:</label>
+                        <input class="form-control" type="text" name="address1"><br>
+                        <label>Plaats:</label>
+                        <input class="form-control" type="text" name="address2"><br>
+                        <label>Land:</label>
+                        <select class="form-control" name="country">
+                            <?php foreach ($array as $key){?>
+                                <option><?php echo $option ?></option>
+                            <?php } ?>
+                        </select><br>
+                        <label>Geboortedatum:</label>
+                        <input class="form-control" type="text" name="city"><br>
+                        <label>E-mail:</label>
+                        <input class="form-control" type="text" name="mail"><br><br>
+>>>>>>> parent of 2c78713... Account page changes
                     <button type="submit" class="btn btn-outline-light my-2 my-sm-0">Verander account informatie</button>
                     <br><br>
                     </div>
                 </form>
+<<<<<<< HEAD
             -<?php }
+=======
+            <?php }
+>>>>>>> parent of 2c78713... Account page changes
             if(($_SESSION['change'] == NULL)){
             ?>
             <br>
