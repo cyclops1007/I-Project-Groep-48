0e759a1f53ab2b6a6a391d2ec8a665927449d47d
AnoukvanVugt <anoukvvugt@hotmail.com>
2018-05-22 14:54:38 +0200

    Footer fix + Database connection PHPdoc

--- Website/EenmaalAndermaal/Database_con.php
+++ Website/EenmaalAndermaal/Database_con.php
@@ -6,16 +6,23 @@
  * Time: 14:13
  */
 
-$server = 'mssql2.iproject.icasites.nl';
-$databaseName = 'iproject48';
-$username = 'iproject48';
-$password = 'TgtHESqUtn';
-
-try{
-    $dbh = new PDO ("sqlsrv:Server=$server;Database=$databaseName;
+/**
+ * Connects the website to the database through the template page.
+ *
+ * @param String $server The server we're trying to connect to
+ * @param String $databaseName Name of the database we're trying to access
+ * @param String $username Username to log into the database
+ * @param String $password Password to match the username
+ * @return void
+ */
+function connectDatabase($server, $databaseName, $username, $password) {
+    try {
+        $dbh = new PDO ("sqlsrv:Server=$server;Database=$databaseName;
     ConnectionPooling=0", "$username", "$password");
-}catch(PDOException $e){
-    die ("Fout met database: {$e->getMessage()}");
+    } catch (PDOException $e) {
+        die ("Fout met database: {$e->getMessage()}");
+    }
 }
 
+connectDatabase('mssql2.iproject.icasites.nl', 'iproject48', 'iproject48', 'TgtHESqUtn');
 ?>
