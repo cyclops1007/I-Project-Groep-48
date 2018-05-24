f5507b2a6a62b107798d1fab02bc2ad5c58cb5dd
cyclops1007 <kayverhaegh1007@gmail.com>
2018-05-24 09:16:50 +0200

    comments voor zooi

--- Website/EenmaalAndermaal/Veilingen_overzicht.php
+++ Website/EenmaalAndermaal/Veilingen_overzicht.php
@@ -10,7 +10,7 @@ include 'Template.php';
 $veiling = veilingen();
 
 if(isset($_SESSION['Zoekplaats'])){
-    calculateDistance($user, $destination, $amountKm, $id);
+    calculateDistance($user, $destination, $amountKm, $id);// hier komt  het id van alle artikelen, de gebruiker postcode de postcode van de random persoon en de hoeveelheid km  dat ie in moet zitten. ja klopt lekker kut.
 }
 ?>
 <!DOCTYPE html>
