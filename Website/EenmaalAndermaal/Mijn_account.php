b001dc8359d4315bb58dea4915e8303720b0f101
cyclops1007 <kayverhaegh1007@gmail.com>
2018-05-24 13:45:33 +0200

    mijn account kut fout gefixd

--- Website/EenmaalAndermaal/Mijn_account.php
+++ Website/EenmaalAndermaal/Mijn_account.php
@@ -54,7 +54,7 @@ if (!isset($_SESSION["change"])) {
                     <input class="form-control" type="text" name="address2" value="<?php echo $key['adresregel2'];?>"><br>
                     <label>Land:</label>
                     <select class="form-control" name="country">
-                        <?php foreach ($array as $key) { ?>
+                        <?php foreach ($array as $option) { ?>
                             <option><?php echo $option; ?></option>
                         <?php } ?>
                     </select><br>
