8462a33cefffae7c5064c87db43c3afc4b08a887
BriantjeU <brianudompol14@gmail.com>
2018-05-24 10:01:59 +0200

    Contact pagina is af

--- Website/EenmaalAndermaal/Contact_pagina.php
+++ Website/EenmaalAndermaal/Contact_pagina.php
@@ -21,6 +21,7 @@ include 'Template.php';
 
 <div id="text-container" class="container rounded col-sm-6">
     <h1>Contact</h1>
+    <img width="320" height="200" src="image/ROCTechnovium.jpg" alt="Fancy building" class="float-right">
     <p>Heeft u als klant,verkoper of gast vragen?<br> Dan kunt u met ons contact opnemen via de volgende gegevens:</p>
     <h2>Adresgegevens</h2>
         <p>     Heyendaalseweg 98
@@ -28,7 +29,7 @@ include 'Template.php';
             <br>Telefoon(024) 890 45 00
             <br>Fax(024) 890 45 23
             <br>Mail: eenmaalandermaal2018@gmail.com
-            <img src="image/ROCTechnovium.jpg" alt="Fancy building" style="float:right; width:300px; height:250px;">
+
     <h2>Postadres</h2>
                 Postbus 655
             <br> 6500 AR Nijmegen
