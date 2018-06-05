3c91746978fbbd40653b37ae1e0b19cfd629d388
AnoukvanVugt <anoukvvugt@hotmail.com>
2018-06-05 11:24:07 +0200

    Javascript Veilingpagina Werkend

--- Website/EenmaalAndermaal/Veiling.php
+++ Website/EenmaalAndermaal/Veiling.php
@@ -15,6 +15,9 @@ $veiling = artikelnummer($veilingId);
 $valuta = valuta($veiling[0]['valuta']);
 $foto = artikelfoto($veilingId);
 $hoogsteBod = getHoogsteBod($veilingId);
+$endTimeArray = getEndDate($veilingId);
+$endTime = $endTimeArray['looptijdEindeDag'] . " " . $endTimeArray['looptijdEindeTijdstip'];
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -46,28 +49,31 @@ $hoogsteBod = getHoogsteBod($veilingId);
         }
 
         //javascript for countdown timer
-        var now = new Date().getTime();
+        var countDownDate = new Date("<?= $endTime ?>").getTime();
 
-        var countDownDate = now.setHours(now.getHours()+12);
-
+        // Update the count down every 1 second
         var x = setInterval(function() {
 
+            // Get todays date and time
+            var now = new Date().getTime();
+
             // Find the distance between now an the count down date
-            var countDownTime = countDownDate - now;
+            var distance = countDownDate - now;
 
-            // Time calculations for hours, minutes and seconds
-            var hours = Math.floor((countDownTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            var minutes = Math.floor((countDownTime % (1000 * 60 * 60)) / (1000 * 60));
-            var seconds = Math.floor((countDownTime % (1000 * 60)) / 1000);
+            // Time calculations for days, hours, minutes and seconds
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-            // Display the result in the element with id="demo"
-            document.getElementById("resterendeVeilingDuur").innerHTML = days + "d " + hours + "h "
+            // Output the result in an element with id="demo"
+            document.getElementById("resterendeVeilingDuur").innerHTML = "Resterende veilingduur: " + days + "d " + hours + "u "
                 + minutes + "m " + seconds + "s ";
 
-            // If the count down is finished, write some text
+            // If the count down is over, write some text
             if (distance < 0) {
                 clearInterval(x);
-                document.getElementById("resterendeVeilingDuur").innerHTML = "GESLOTEN";
+                document.getElementById("resterendeVeilingDuur").innerHTML = "Resterende veilingduur: GESLOTEN";
             }
         }, 1000);
     </script>
@@ -120,8 +126,9 @@ $hoogsteBod = getHoogsteBod($veilingId);
                 <div class = row>
                     <div class="col-sm-6">
                         <h1>Titel</h1>
-                        <p>Orginele prijs:<?php echo "$valuta" . $veiling[0]['startprijs'];?></p>
-                        <p id="hoogsteBod">Huidige prijs:<?php echo "$valuta" . $hoogsteBod[0];?></p>
+                        <p>Orginele prijs: <?php echo "$valuta" . $veiling[0]['startprijs'];?></p>
+                        <p id="hoogsteBod">Huidige prijs: <?php echo "$valuta" . $hoogsteBod[0];?></p>
+                        <p><?= $_POST['bod']; ?></p>
                         <p id="resterendeVeilingDuur"></p>
                         <br>
                         <?php if(isset($_SESSION['rol']) && $_SESSION['rol'] != 0){ ?>
