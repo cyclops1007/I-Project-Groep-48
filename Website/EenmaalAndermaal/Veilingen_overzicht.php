f7ec47348329d3137c94937395caa78fbf3e94e9
BriantjeU <brianudompol14@gmail.com>
2018-06-05 11:55:18 +0200

    Pagisation

--- Website/EenmaalAndermaal/Veilingen_overzicht.php
+++ Website/EenmaalAndermaal/Veilingen_overzicht.php
@@ -1,4 +1,4 @@
-<?php
+-<?php
 /**
  * Created by PhpStorm.
  * User: joey-
@@ -9,63 +9,7 @@
 include 'Template.php';
 $veiling = veilingen();
 
-try {
 
-    // Het aantal voorwerpen in de tabel
-    $total = $dbh->query('
-        SELECT COUNT(*) FROM Voorwerp')->fetchColumn();
-
-    // Aantal veilingen per pagina
-    $limit = 20;
-
-    // Het aantal paginas dat er komen
-    $pages = ceil($total / $limit);
-
-    // weergeeft de huidige pagina
-    $page = min($pages, filter_input(INPUT_GET, 'page', FILTER_VALIDATE_INT, array(
-        'options' => array(
-            'default'   => 1,
-            'min_range' => 1,
-        ),
-    )));
-
-    // Berekent de offset van de query
-    $offset = ($page - 1)  * $limit;
-
-    // Some information to display to the user
-    $start = $offset + 1;
-    $end = min(($offset + $limit), $total);
-
-    // The "back" link
-    $prevlink = ($page > 1) ? '<a href="?page=1" title="First page">&laquo;</a> <a href="?page=' . ($page - 1) . '" title="Previous page">&lsaquo;</a>' : '<span class="disabled">&laquo;</span> <span class="disabled">&lsaquo;</span>';
-
-    // The "forward" link
-    $nextlink = ($page < $pages) ? '<a href="?page=' . ($page + 1) . '" title="Next page">&rsaquo;</a> <a href="?page=' . $pages . '" title="Last page">&raquo;</a>' : '<span class="disabled">&rsaquo;</span> <span class="disabled">&raquo;</span>';
-
-    // Display the paging information
-    echo '<div id="paging"><p>', $prevlink, ' Page ', $page, ' of ', $pages, ' pages, displaying ', $start, '-', $end, ' of ', $total, ' results ', $nextlink, ' </p></div>';
-
-    // Prepare the paged query
-    $stmt = $dbh->prepare("SELECT voorwerpnummer From Voorwerp ORDER BY titel 
-                                OFFSET $offset ROWS
-                                FETCH NEXT $limit ROWS ONLY");
-    $stmt->execute();
-    $rows = $stmt->fetchAll(pdo::FETCH_ASSOC);
-
-
-    // Do we have any results?
-    if ($rows > 0) {
-        // Define how we want to fetch the results
-        $iterator = new IteratorIterator($stmt);
-
-
-    } else {
-        echo '<p>No results could be displayed.</p>';
-    }
-
-} catch (Exception $e) {
-    echo '<p>', $e->getMessage(), '</p>';
-}
 ?>
 <!DOCTYPE html>
 <html lang="nl">
@@ -85,17 +29,75 @@ try {
     </thead>
     <tbody>
     <?php
-    foreach ($veiling as $key) {
-        ?>
-        <tr>
-            <th scope="col"><a href="veiling.php?<?php echo $key['voorwerpnummer']?>">   <?php echo $key['titel']; ?></a></th>
-            <td><img src="<?php echo 'http://iproject5.icasites.nl/thumbnails/' . $key['thumbnail']; ?>"></td>
-            <td><?php echo $key['beschrijving']; ?></td>
-            <td><?php echo $key['startprijs'] . ',00'; ?></td>
-            <td><?php echo $key['verkoper']; ?></td>
-        </tr>
-        <?php
+    try {
+
+        // Het aantal voorwerpen in de tabel
+        $total = $dbh->query('
+        SELECT COUNT(*) FROM Voorwerp')->fetchColumn();
+
+        // Aantal veilingen per pagina
+        $limit = 20;
+
+        // Het aantal paginas dat er komen
+        $pages = ceil($total / $limit);
+
+        // weergeeft de huidige pagina
+        $page = min($pages, filter_input(INPUT_GET, 'page', FILTER_VALIDATE_INT, array(
+            'options' => array(
+                'default'   => 1,
+                'min_range' => 1,
+            ),
+        )));
+
+        // Berekent de offset van de query
+        $offset = ($page - 1)  * $limit;
+
+        // Some information to display to the user
+        $start = $offset + 1;
+        $end = min(($offset + $limit), $total);
+
+        // The "back" link
+        $prevlink = ($page > 1) ? '<a href="?page=1" title="First page">&laquo;</a> <a href="?page=' . ($page - 1) . '" title="Previous page">&lsaquo;</a>' : '<span class="disabled">&laquo;</span> <span class="disabled">&lsaquo;</span>';
+
+        // The "forward" link
+        $nextlink = ($page < $pages) ? '<a href="?page=' . ($page + 1) . '" title="Next page">&rsaquo;</a> <a href="?page=' . $pages . '" title="Last page">&raquo;</a>' : '<span class="disabled">&rsaquo;</span> <span class="disabled">&raquo;</span>';
+
+        // Display the paging information
+        echo '<div id="paging"><p>', $prevlink, ' Page ', $page, ' of ', $pages, ' pages, displaying ', $start, '-', $end, ' of ', $total, ' results ', $nextlink, ' </p></div>';
+
+        // Prepare the paged query
+        $stmt = $dbh->prepare("SELECT * From Voorwerp ORDER BY titel 
+                                OFFSET $offset ROWS
+                                FETCH NEXT $limit ROWS ONLY");
+        $stmt->execute();
+        $rows = $stmt->fetchAll(pdo::FETCH_ASSOC);
+
+
+        // Do we have any results?
+        if ($rows > 0) {
+            // Define how we want to fetch the results
+            $iterator = new IteratorIterator($stmt);
+            foreach ($rows as $key) {
+                ?>
+                <tr>
+                    <th scope="col"><a href="veiling.php?<?php echo $key['voorwerpnummer']?>">   <?php echo $key['titel']; ?></a></th>
+                    <td><img src="<?php echo 'http://iproject5.icasites.nl/thumbnails/' . $key['thumbnail']; ?>"></td>
+                    <td><?php echo $key['beschrijving']; ?></td>
+                    <td><?php echo $key['startprijs'] . ',00'; ?></td>
+                    <td><?php echo $key['verkoper']; ?></td>
+                </tr>
+                <?php
+            }
+
+
+        } else {
+            echo '<p>No results could be displayed.</p>';
+        }
+
+    } catch (Exception $e) {
+        echo '<p>', $e->getMessage(), '</p>';
     }
+
     ?>
     </tbody>
 </table>
